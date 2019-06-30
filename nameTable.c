8396f8d25ad7b0664ac545239904b1ba5800cae3
Alexandr <alexandr539XXL@mail.ru>
2019-06-30 15:48:10 +0300

    Поправил отступы

--- nameTable.c
+++ nameTable.c
@@ -3,28 +3,28 @@
 #include <string.h>
 
 struct entry {
-  struct entry *next;
-  const char *name;
-  uint16_t value;
+	struct entry *next;
+	const char *name;
+	uint16_t value;
 };
 
 struct entry *nameTable = 0;
 
 int nameTableAdd (const char *name, uint16_t value) {
-  if (0 == nameTableSearch (name, 0))
-    return 2; // метка уже присутствует в таблице символов
-  struct entry *newEntry = (struct entry *)calloc (sizeof (struct entry));
-  if (newEntry == 0) 
-    return 1; //не удалось выделить память
-  newEntry->name = strdup (name);
-  if (newEntry->name == 0) {
-    free (newEntry);
-    return 1; //не удалось присвоить имя 
-  }
-  newEntry->value = value;
-  newEntry->next = nameTable;
-  nameTable = newEntry;
-  return 0; // метка успешно добавлена в таблицу символов
+	if (0 == nameTableSearch (name, 0))
+		return 2; // метка уже присутствует в таблице символов
+	struct entry *newEntry = (struct entry *)calloc (sizeof (struct entry));
+	if (newEntry == 0) 
+		return 1; //не удалось выделить память
+	newEntry->name = strdup (name);
+	if (newEntry->name == 0) {
+		free (newEntry);
+		return 1; //не удалось присвоить имя 
+	}
+	newEntry->value = value;
+	newEntry->next = nameTable;
+	nameTable = newEntry;
+	return 0; // метка успешно добавлена в таблицу символов
 }
 
 int nameTableSearch (const char *name, uint16_t *valueDst) {
